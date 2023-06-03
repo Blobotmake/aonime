21d654eb9312aa267ccbd1c6e805738024d1d025
Aryan sharma <125473083+Blobotmake@users.noreply.github.com>
2023-06-03 16:20:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,7 +49,6 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'FILES')
 
 # Others 
 ALINK = environ.get('ALINK', 'AKD_ANIME')
-SPLINK = environ.get('SPLINK', 'Suport_Badsha_Studios')
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "10")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
