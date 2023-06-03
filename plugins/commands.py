cdb67157480105e3c9eb0caf31ff9934cc6bbc74
Aryan sharma <125473083+Blobotmake@users.noreply.github.com>
2023-06-03 16:18:25 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -2,7 +2,7 @@ import os
 import logging
 import random
 import asyncio 
-from info import SPLINK, ALINK
+from info import ALINK
 from Script import script
 from pyrogram import Client, filters, enums
 from pyrogram.errors import ChatAdminRequired, FloodWait
